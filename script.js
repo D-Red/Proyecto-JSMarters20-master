fc6c6581a0e8ee55435608d9d696d75905f0a76b
D-Red <2012.cabeza.gonzalez@gmail.com>
2025-05-26 23:12:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const students=[];
+
+document.getElementById("studentForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const name=document.getElementById("name").value.trim();
+    const lastName=document.getElementById("lastname").value.trim();
+    const grade=parseFloat(document.getElementById("grade").value);
+    const date = document.getElementById("date").value;
+
+    if (!name || !lastName || isNaN(grade) || grade < 1 || grade > 7 || !date) {
+        alert("Error: Datos Incorrectos");
+        return;
+    }
+
+    const student={name,lastName,grade}
+    students.push(student)
+    //console.log(students)
+    addStudentToTable(student);
+    calcularPromedio();
+    this.reset()
+
+});
+const tableBody=document.querySelector('#studentsTable tbody');
+function addStudentToTable(student){
+    const row=document.createElement("tr");
+    row.innerHTML=`
+    <td>${student.name}</td>
+    <td>${student.lastName}</td>
+    <td>${student.grade}</td>
+    <td>${student.date}</td>
+    <td>
+        <button class="edit-btn">Editar</button>
+        <button class="delete-btn">Eliminar</button>
+    </td>
+    `;
+
+    // Botón para Eliminar
+    row.querySelector(".delete-bth").addEventListener("click",function(){
+        borrarEstudiante(student,row);
+    });
+
+    // Botón editar
+    row.querySelector(".edit-btn").addEventListener("click", function () {
+        editarEstudiante(student, row);
+    });    
+
+    tableBody.appendChild(row);
+}
+function borrarEstudiante(student,row){
+    const index=students.indexOf(student);
+    if(index > -1){
+        students.splice(index,1);
+    }
+    row.remove();
+    calcularPromedio();
+}
+
+function editarEstudiante(student, row) {
+    // Rellenar el formulario con los datos del estudiante
+    document.getElementById("name").value = student.name;
+    document.getElementById("lastname").value = student.lastName;
+    document.getElementById("grade").value = student.grade;
+    document.getElementById("date").value = student.date;
+
+    // Eliminar el estudiante actual y su fila
+    borrarEstudiante(student, row);
+}
+
+const averageDiv=document.getElementById("average");
+
+function calcularPromedio(){
+    let suma = 0;
+    for (const student of students){
+        suma += student.grade;
+    }
+    const count = students.length;
+    const promedio = suma / count;
+    console.log(promedio);
+    averageDiv.textContent = "Promedio General del Curso :" +promedio;
+
+}
